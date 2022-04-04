4c603a768a356d936ce247d7c34e74f908fad5ba
MehruzSaif <mehruzsaif007@gmail.com>
2022-04-04 14:48:42 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import Reviews from './components/Reviews/Reviews';
+import Dashboard from './components/Dashboard/Dashboard';
+import Blogs from './components/Blogs/Blogs';
+import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-6xl'>Welcome to my pricing club</h1>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home />}>HOME</Route>
+        <Route path='/reviews' element={<Reviews />}>REVIEWS</Route>
+        <Route path='/dashboard' element={<Dashboard />}>DASHBOARD</Route>
+        <Route path='/blogs' element={<Blogs />}>BLOGS</Route>
+        <Route path='/about' element={<About />}>ABOUT</Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Navbar = () => {
+    const routes = [
+        { id: 1, }
+    ]
+
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
