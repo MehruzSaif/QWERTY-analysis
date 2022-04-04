6956db0ac1cf02f2b21ae66541175769c5ea0091
MehruzSaif <mehruzsaif007@gmail.com>
2022-04-04 18:00:23 +0600

    added home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import CustomLink from '../CustomLink/CustomLink';
+import './Header.css';
 
 const Header = () => {
     return (
         <div>
-            <nav className='navbar'>
-                <CustomLink to="/">HOME</CustomLink>
-                <CustomLink to="/reviews">REVIEWS</CustomLink>
-                <CustomLink to="/dashboard">DASHBOARD</CustomLink>
-                <CustomLink to="/blogs">BLOGS</CustomLink>
-                <CustomLink to="/about">ABOUT</CustomLink>
+            <nav className='container nav-bar mt-3'>
+                <CustomLink className='custom-link' to="/">HOME</CustomLink>
+                <CustomLink className='custom-link' to="/reviews">REVIEWS</CustomLink>
+                <CustomLink className='custom-link' to="/dashboard">DASHBOARD</CustomLink>
+                <CustomLink className='custom-link' to="/blogs">BLOGS</CustomLink>
+                <CustomLink className='custom-link' to="/about">ABOUT</CustomLink>
             </nav>
         </div>
     );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Home.css';
+import img1 from '../images/keyboard-home.png';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is home</h2>
+        <div className='product-container my-5'>
+            <div className="written-container">
+                <h1>Your next <span>CONSOLE</span></h1>
+                <h1>Your best <span>QWERTY</span></h1>
+                <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur similique officia quis quasi minima, odit, hic adipisci veritatis delectus eius ullam tempora asperiores, totam pariatur perspiciatis inventore. Illum, dolor non!</h5>
+            </div>
+
+            <div className="img-container">
+                <img src={img1} alt="" />
+            </div>
         </div>
     );
 };
